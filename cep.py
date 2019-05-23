b9303c5d22540ce99f6bdda7f90b23d0218a2b3f
HumamKourani <kourani.de@gmail.com>
2019-05-23 11:49:26 +0200

    I have organized the work in 4 files (instead of the initial code in Script.py) and I have implemented the following functions: - in length_window: the functions last_len, first_len, last_event and the corresponding observers - in time_window: the function last_time and the corresponding observer The class DataframeManager is also modified a bit

---
+++ cep.py
@@ -0,0 +1,32 @@
+from random import randrange
+from DataframeManager import *
+from length_window import last_event, last_len, first_len
+from time_window import last_time
+
+
+def avg_price_last_30_minutes():
+    avg_price = last_time(minutes=30)['price'].mean()
+    #print(all_dfs['last_time_30m'].dataframe)
+    print('mean in the last 30 minutes: ' + str(avg_price))
+
+
+def avg_price_last_two_events_observer():
+    avg_price = last_len(2)['price'].mean()
+    #print(all_dfs['last_len_2'].dataframe)
+    if avg_price > 6:
+            print('The average of the last two events is: ' + str(avg_price))
+
+
+def test_first_five_events_observer():
+    a = first_len(5)
+    print(all_dfs['first_len_5'].dataframe)
+
+
+all_dfs['StockTick'] = DataframeManager(['symbol', 'price'])
+all_dfs['StockTick'].observers.update({avg_price_last_two_events_observer : []})
+all_dfs['StockTick'].observers.update({test_first_five_events_observer : []})
+all_dfs['StockTick'].observers.update({avg_price_last_30_minutes : []})
+
+
+for i in range(20):
+    all_dfs['StockTick'].add({'symbol' : 'AAPL', 'price' : float(randrange(1, 10))})
