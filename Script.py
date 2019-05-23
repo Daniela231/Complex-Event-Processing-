b9e75090b07e90802f1b262942cde0f35d2fa20d
Daniela231 <daniela.petcu@rwth-aachen.de>
2019-05-23 17:44:17 +0200

    Merge branch 'master' of C:\Users\Daniela\PycharmProjects\Complex-Event-Processing with conflicts.

--- Script.py
+++ Script.py
@@ -1,3 +1,8 @@
+from pm4py.algo.discovery.alpha import factory as alpha_miner
+from pm4py.objects.log.importer.xes import factory as xes_importer
+from pm4py.visualization.petrinet import factory as vis_factory
+
+
 from random import randrange
 import numpy as np
 import pandas as pd
