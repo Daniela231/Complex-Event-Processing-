86c40c03cd2464d52be11ca61c7fa5757c197a6f
Fabien Saint Preux <fabien.saint.preux@rwth-aachen.de>
2019-05-23 16:27:39 +0200

    Loading Sorting Script

---
+++ event_log_loading_sorting.py
@@ -0,0 +1,8 @@
+from pm4py.objects.log.importer.xes import factory as xes_importer
+from pm4py.objects.conversion.log import factory as conv_factory
+from pm4py.objects.log.util import sorting
+
+log = xes_importer.apply("C:/receipt.xes")
+stream = conv_factory.apply(log)
+stream = sorting.sort_timestamp(stream)
+print(stream)
