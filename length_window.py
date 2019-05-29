5b4feffde682c28bfa050f85c5b99817ac900c3b
HumamKourani <kourani.de@gmail.com>
2019-05-29 12:39:30 +0200

    the function length_batch is added

--- length_window.py
+++ length_window.py
@@ -17,11 +17,24 @@ def first_unique(*param):
     return all_dfs["StockTick"].dataframe.drop_duplicates(subset=param, keep='first', inplace=True)
 
 
-def sort(size, criteria):
-    count = 1
-    while count<size:
-        all_dfs["StockTick"].dataframe.sort_values(by=criteria, ascending=1)
-        count += 1
+def sort_observer(key, size ,criteria):
+    all_dfs[key].dataframe = all_dfs[key].dataframe.append(last_event()).sort_values(by=[elm[0] for elm in criteria], ascending=[elm[1] for elm in criteria]).head(size)
+    return False
+
+
+def sort(size ,criteria):
+    key = 'sort_'+str(size)
+    for elm, symbol in criteria:
+        key = key +'_'+str(elm)+'_'+str(symbol)
+
+    if key not in all_dfs.keys():
+        all_dfs[key] = DataframeManager()
+        all_dfs[key].dataframe = all_dfs["StockTick"].dataframe.sort_values(by=[elm[0] for elm in criteria], ascending=[elm[1] for elm in criteria]).head(size)
+        all_dfs[key].observers.update({sort_observer : [key, size ,criteria]})
+    else:
+        all_dfs[key].add_df(last_event())
+
+    return all_dfs[key].dataframe
 
 
 def last_length_observer(key, len):
