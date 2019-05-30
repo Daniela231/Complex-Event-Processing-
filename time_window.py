2a9db906318e0f14fe63c34d6b1906c24155492a
Fabien Saint Preux <fabien.saint.preux@rwth-aachen.de>
2019-05-30 15:31:48 +0200

    ext time batch

--- time_window.py
+++ time_window.py
@@ -71,7 +71,7 @@ def first_time(weeks=0, days=0, hours=0, minutes=0, seconds=0, milliseconds=0, m
 result = None
 result_available = threading.Event()
 
-def time_batch_observer(time):#
+def time_batch_observer(time):
     if(np.datetime64('now') < time):
         result = np.datetime64('now') < time
         result_available.set()
@@ -91,5 +91,35 @@ def time_batch(weeks=0, days=0, hours=0, minutes=0, seconds=0, milliseconds=0, m
         thread = threading.Thread(target=time_batch_observer)
         thread.start()
         result_available.wait()
+     return True
 
 
+def ext_time_batch_observer(lasttime,time):
+    return lasttime < time
+
+def ext_time_batch(weeks=0, days=0, hours=0, minutes=0, seconds=0, milliseconds=0, microseconds=0, nanoseconds=0):
+    continueBatch = True
+    while continueBatch:
+        dict = locals()
+        key = 'ext_time'
+        time = list(all_dfs['StockTick'].dataframe['INSERTION_TIMESTAMP'])[0]
+        for i in dict.keys():
+            if dict[i] != 0:
+                time = time + np.timedelta64(int(dict[i]), abbreviations[i])
+                key = key + '_' + str(dict[i]) + abbreviations[i]
+
+        if key not in all_dfs.keys():
+            all_dfs[key] = DataframeManager()
+            all_dfs[key].dataframe = all_dfs["StockTick"].dataframe[
+                all_dfs["StockTick"].dataframe['INSERTION_TIMESTAMP'] < time]
+            all_dfs[key].observers.update({ext_time_batch_observer: [all_dfs['StockTick'].dataframe['INSERTION_TIMESTAMP'],time]})
+        else:
+            all_dfs[key].add_df(last_event())
+
+        x = input("Continue? (y/n)")
+        if x == "y":
+            continueBatch = True
+        else:
+            continueBatch = False
+
+    return all_dfs[key].dataframe
