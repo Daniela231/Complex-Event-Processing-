149fe069e84bfe28c9abac40a7c2829ffb4f930b
Sajujin <mike.holtzem@rwth-aachen.de>
2019-05-30 15:55:30 +0200

    added simple logger into .log file for observers dataframe output

--- cep.py
+++ cep.py
@@ -5,7 +5,7 @@ from time_window import *
 
 import logging
 l=logging.getLogger("test")
-f=logging.FileHandler("test.log", mode='w')
+f=logging.FileHandler("ceplogfile.log", mode='w')
 l.addHandler(f)
 s=logging.StreamHandler()
 l.addHandler(s)
@@ -13,15 +13,15 @@ l.addHandler(s)
 
 def avg_price_last_30_minutes():
     avg_price = last_time(minutes=30)['price'].mean()
-    print(all_dfs['last_time_30m'].dataframe)
-    print('mean in the last 30 minutes: ' + str(avg_price))
+    l.critical(all_dfs['last_time_30m'].dataframe)
+    l.critical('mean in the last 30 minutes: ' + str(avg_price))
     return False
 
 
 def avg_price_last_two_events_observer():
     avg_price = last_len(2)['price'].mean()
-    print(all_dfs['last_len_2'].dataframe)
-    print(size(all_dfs['last_len_2']))
+    l.critical(all_dfs['last_len_2'].dataframe)
+    l.critical(size(all_dfs['last_len_2']))
     if avg_price > 6:
             print('The average of the last two events is: ' + str(avg_price))
     return False
@@ -29,14 +29,14 @@ def avg_price_last_two_events_observer():
 
 def test_first_five_events_observer():
     a = first_len(5)
-    print(all_dfs['first_len_5'].dataframe)
-    print(size(all_dfs['first_len_5']))
+    l.critical(all_dfs['first_len_5'].dataframe)
+    l.critical(size(all_dfs['first_len_5']))
     return False
 
 
 def test_first_time_observer():
     a = first_time(nanoseconds = 2)
-    print(all_dfs['first_time_2ns'].dataframe)
+    l.critical(all_dfs['first_time_2ns'].dataframe)
     return False
 
 
@@ -55,16 +55,16 @@ def test_sort():
 
 
 all_dfs['StockTick'] = DataframeManager()
-#all_dfs['StockTick'].observers.update({avg_price_last_two_events_observer : []})
+all_dfs['StockTick'].observers.update({avg_price_last_two_events_observer : []})
 #all_dfs['StockTick'].observers.update({test_first_five_events_observer : []})
 #all_dfs['StockTick'].observers.update({avg_price_last_30_minutes : []})
 #all_dfs['StockTick'].observers.update({test_first_time_observer : []})
-all_dfs['StockTick'].observers.update({test_length_batch : []})
+#all_dfs['StockTick'].observers.update({test_length_batch : []})
 #all_dfs['StockTick'].observers.update({test_sort : []})
 
 
 #22min for 75000k (23.5.2019)
-for i in range(20):
+for i in range(400):
     p = float(randrange(1, 10))
     #p=i
     print(p)
